55b71856da2926bfdeaf5ac8b0c5af9386075589
PenginPenPen <92212932+PenginPenPen@users.noreply.github.com>
2023-03-13 19:08:01 +0900

    _

--- Vigenère_cipher.py
+++
@@ -1,9 +0,0 @@
-# import string
-# komoji = list(string.ascii_lowercase)
-# texts, keys = input('ここに文字と暗号鍵を入力してください').split()
-# for i in range(len(texts)):
-#     for key in keys:
-#         key_position= komoji.index(key)
-#     for text in texts:
-#         text_position= komoji.index(text)
-#むずい
