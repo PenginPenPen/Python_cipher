88778af5a13655a7cebac661204a2feb801782b4
PenginPenPen <92212932+PenginPenPen@users.noreply.github.com>
2023-03-15 10:59:35 +0900

    update
    inputの表示に例を追加しました。

--- Caesar_cipher.py
+++ Caesar_cipher.py
@@ -1,6 +1,6 @@
 import string
 komoji = list(string.ascii_lowercase)
-text, key = input('ここに文字と暗号鍵を入力してください').split()
+text, key = input('ここに文字と暗号鍵を入力してください例:abc 3').split()
 for i in text:
     if i in komoji:
         position = komoji.index(i)
